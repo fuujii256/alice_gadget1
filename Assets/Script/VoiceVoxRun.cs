7e52a3fb5d9098d97c197c799a43ff975e7f84cf
fuujii256 <yoshihiro.fujita256@outlook.jp>
2024-01-12 11:27:01 +0900

    first Commit

---
+++ Assets/Script/VoiceVoxRun.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class VoiceVoxRun : MonoBehaviour
+{
+    [SerializeField]
+    
+    public GameObject alice_Text;
+
+    private string viewText;
+    public AudioSource _audioSource;
+    
+
+    void Start()
+    {
+        viewText = "先生、こんにちは！、アリスは今日も元気です！";
+        StartCoroutine(SpeakTest(viewText));
+        //テキストを表示
+        alice_Text.GetComponent<Text>().text = viewText;
+    }
+
+    IEnumerator SpeakTest (string text)
+    {
+        // VOICEVOXのREST-APIクライアント
+        VoiceVoxApiClient client = new VoiceVoxApiClient();
+
+        // テキストからAudioClipを生成（話者は「8:春日部つむぎ」）
+        yield return client.TextToAudioClip(58, text);
+
+        if (client.AudioClip != null)
+        {
+            // AudioClipを取得し、AudioSourceにアタッチ
+            _audioSource.clip = client.AudioClip;
+            // AudioSourceで再生
+            _audioSource.Play();
+        }
+    }
+}
